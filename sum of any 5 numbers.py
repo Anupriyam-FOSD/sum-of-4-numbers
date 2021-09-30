541c42312690ce20bdd76f897bdbb6ade0cf4cd9
Anupriyam Shukla <60869367+Anupriyam-FOSD@users.noreply.github.com>
2021-09-30 20:59:41 +0530

    Sum of 4 numbers

---
+++ sum of any 5 numbers.py
@@ -0,0 +1,11 @@
+print("Welcome !!!")
+
+Num1 = float(input("Enter the first number :"))
+Num2 = float(input("Enter the second number :"))
+Num3 = float(input("Enter the third number :"))
+Num4 = float(input("Enter the fourth number :"))
+
+
+Sum= Num1+Num2+Num3+Num4
+
+print("Hello")
