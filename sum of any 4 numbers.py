9405195b5c459307771ccf972512bbd845e8785d
Ayush Singh <aayushsingh.code@gmail.com>
2021-09-30 21:37:11 +0530

    renamed file and updated code

---
+++ sum of any 4 numbers.py
@@ -0,0 +1,14 @@
+print("Welcome !!!")
+print("This app allows you to enter 5 number and find sum of it")
+print("Press enter to continue")
+input("")
+
+num1 = float(input("Enter the first number :"))
+num2 = float(input("Enter the second number :"))
+num3 = float(input("Enter the third number :"))
+num4 = float(input("Enter the fourth number :"))
+
+
+sum= num1+num2+num3+num4
+
+print(sum)
