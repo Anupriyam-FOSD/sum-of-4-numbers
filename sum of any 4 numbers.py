a7cc3b41e39ef0844e7d8d452a5f6efb4101542a
noebou <91731402+noebou@users.noreply.github.com>
2021-10-01 12:48:44 +0200

    Catch ValueError and

--- sum of any 4 numbers.py
+++ sum of any 4 numbers.py
@@ -1,14 +1,15 @@
 print("Welcome !!!")
-print("This app allows you to enter 5 number and find sum of it")
+print("This app allows you to enter 4 number and find sum of it")
 print("Press enter to continue")
 input("")
 
-num1 = float(input("Enter the first number :"))
-num2 = float(input("Enter the second number :"))
-num3 = float(input("Enter the third number :"))
-num4 = float(input("Enter the fourth number :"))
-
-
-sum= num1+num2+num3+num4
-
-print(sum)
+try:
+    num1 = float(input("Enter the first number :"))
+    num2 = float(input("Enter the second number :"))
+    num3 = float(input("Enter the third number :"))
+    num4 = float(input("Enter the fourth number :"))
+    sum = num1 + num2 + num3 + num4
+except ValueError:
+    print("There was an error while calculating!")
+else:
+    print(sum)
